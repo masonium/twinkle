911e3d38e04bf977c28b5c1419416d1289bb7f2f
Mason Smith <masonium@masonium.com>
2015-06-25 02:08:45 -0700

    Adds -Wextra warnings

--- header.mk
+++ header.mk
@@ -1,5 +1,6 @@
 CXX_VERSION = -std=c++1y
-SFLAGS = -Wall -fsyntax-only $(CXX_VERSION)
-LFLAGS =  -pthread -lm  -lUnitTest++
-CXXFLAGS = -Wall $(CXX_VERSION) -g -Isrc/ -Itests/ -Iextlib/
+COMMON_FLAGS = -Wall -Wextra -Wno-unused-parameter $(CXX_VERSION)
+SFLAGS =  -fsyntax-only $(COMMON_FLAGS)
+LFLAGS = -pthread -lm  -lUnitTest++
+CXXFLAGS = $(COMMON_FLAGS) -g -Isrc/ -Itests/ -Iextlib/
 #CXXFLAGS = -Wall $(CXX_VERSION) -O3 -Isrc/ -Itests/ -Iextlib/
