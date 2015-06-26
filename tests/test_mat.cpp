b3ccf4e4cf6cc2dc520130fd8c58d39837ed477f
Mason Smith <masonium@gmail.com>
2015-06-26 10:16:42 -0700

    Moves vector, matrix testing into associated files

--- tests/test_mat.cpp
+++ tests/test_mat.cpp
@@ -4,31 +4,81 @@
 
 namespace
 {
-  TEST(inverse)
-  {
-    Mat33 eye = Mat33::identity;
-    CHECK_MAT(eye, eye.inverse());
-    for (int i = 0; i < 20; ++i)
-    {
-      scalar angle = rf() * 2 * PI;
-      auto axis = random_normal();
-      Mat33 r = Mat33::from_axis_angle(axis, angle);
 
-      CHECK_MAT(eye, r * r.inverse());
+  SUITE(Mat)
+  {
+    TEST(transpose)
+    {
+      Mat33 x{1, 2, 3, 4, 5, 6, 7, 8, 9};
+      Mat33 xt{1, 4, 7, 2, 5, 8, 3, 6, 9};
+      CHECK_MAT(xt, x.transpose());
+    }
+
+    TEST(rotate_z)
+    {
+      Mat33 r1 = Mat33::rotate_match( Vec3(0.5, 0.5, sqrt(0.5)), Vec3::z_axis );
+      Mat33 r2 = Mat33::rotate_to_z( Vec3(0.5, 0.5, sqrt(0.5)) );
+      CHECK_MAT(r1, r2);
+    }
+
+    TEST(rotate_rand)
+    {
+      for (int i = 0; i < 100; ++i)
+      {
+        Vec3 t1 = random_normal();
+        Vec3 t2 = random_normal();
+        Mat33 r1 = Mat33::rotate_match( t1, t2);
+        CHECK_VEC( t2, r1 * t1 );
+      }
+    }
+
+    TEST(rotate)
+    {
+      Mat33 r1 = Mat33::rotate_match( Vec3::x_axis, Vec3::y_axis );
+      Mat33 r2 = Mat33::from_axis_angle( Vec3::z_axis, PI/2.0 );
+      CHECK_MAT(r1, r2);
+      CHECK_VEC(Vec3::y_axis, r1 * Vec3::x_axis);
+
+      {
+        Vec3 target(sqrt(0.5), sqrt(0.5), 0);
+        Mat33 r1 = Mat33::rotate_match( Vec3::x_axis, target );
+        Mat33 r2 = Mat33::from_axis_angle( Vec3::z_axis, PI/4.0 );
+        CHECK_MAT(r1, r2);
+        CHECK_VEC(target, r1 * Vec3::x_axis);
+      }
+    }
+
+    TEST(rotate_z_to_z)
+    {
+      Mat33 eye = Mat33::identity;
+      Mat33 m = Mat33::rotate_match(Vec3::z_axis, Vec3::z_axis);
+      CHECK_MAT(eye, m);
+    }
+    TEST(inverse)
+    {
+      Mat33 eye = Mat33::identity;
+      CHECK_MAT(eye, eye.inverse());
+      for (int i = 0; i < 20; ++i)
+      {
+        scalar angle = rf() * 2 * PI;
+        auto axis = random_normal();
+        Mat33 r = Mat33::from_axis_angle(axis, angle);
+
+        CHECK_MAT(eye, r * r.inverse());
+      }
+    }
+    TEST(determinant)
+    {
+      Mat33 eye = Mat33::identity;
+      CHECK_CLOSE(1.0, eye.det(), EPS);
+
+      for (int i = 0; i < 20; ++i)
+      {
+        scalar angle = rf() * 2 * PI;
+        auto axis = random_normal();
+        Mat33 r = Mat33::from_axis_angle(axis, angle);
+        CHECK_CLOSE(1.0, r.det(), EPS);
+      }
     }
   }
-  TEST(determinant)
-  {
-    Mat33 eye = Mat33::identity;
-    CHECK_CLOSE(1.0, eye.det(), EPS);
-
-    for (int i = 0; i < 20; ++i)
-    {
-      scalar angle = rf() * 2 * PI;
-      auto axis = random_normal();
-      Mat33 r = Mat33::from_axis_angle(axis, angle);
-      CHECK_CLOSE(1.0, r.det(), EPS);
-    }
-  }
-
 }
---
+++ tests/test_vec.cpp
@@ -0,0 +1,117 @@
+#include "test_util.h"
+#include "math_util.h"
+
+namespace
+{
+
+    SUITE(Vec3)
+  {
+    class VecFixture
+    {
+    public:
+      VecFixture() : x{0.0, 1.0, 2.0}, y{1.0}, z{} {}
+
+      Vec3 x, y, z;
+    };
+
+
+    TEST_FIXTURE(VecFixture, Vec3_add)
+    {
+      CHECK_ARRAY_CLOSE((x+y).v, Vec3(1.0, 2.0, 3.0).v, 3, EPS);
+      CHECK_ARRAY_CLOSE((x+z).v, Vec3(0.0, 1.0, 2.0).v, 3, EPS);
+    }
+
+    TEST_FIXTURE(VecFixture, Vec3_sub)
+    {
+      CHECK_ARRAY_CLOSE((x-y).v, Vec3(-1.0, 0.0, 1.0).v, 3, EPS);
+      CHECK_ARRAY_CLOSE((x-z).v, Vec3(0.0, 1.0, 2.0).v, 3, EPS);
+    }
+
+    TEST_FIXTURE(VecFixture, Vec3_scalar_mult)
+    {
+      CHECK_ARRAY_CLOSE( (x * 2.0).v, Vec3(0.0, 2.0, 4.0).v, 3, PRECISE_EPS);
+    }
+
+    TEST_FIXTURE(VecFixture, Vec3_scalar_div)
+    {
+      CHECK_ARRAY_CLOSE( (x / 2.0).v, Vec3(0.0, 0.5, 1.0).v, 3, PRECISE_EPS);
+    }
+
+    TEST(Vec3_rotate_aa_none)
+    {
+      for (int i = 0; i < 10; ++i)
+      {
+        Vec3 axis = random_normal();
+        Vec3 target = random_normal() * (rf() + 0.01) * 5.0;
+        CHECK_ARRAY_CLOSE(target.v, target.rotateAxisAngle(axis, 0).v,
+                          3, PRECISE_EPS);
+      }
+    }
+
+    TEST(Vec3_rotate_aa_180)
+    {
+      const Vec3 z_axis{0.0, 0.0, 1.0};
+      for (int i = 0; i < 10; ++i)
+      {
+        Vec3 target = Vec3{rus(), rus(), 1.0} * (rf() + 0.01) * 5.0;
+        CHECK_ARRAY_CLOSE(Vec3(-target.x, -target.y, target.z).v,
+                          target.rotateAxisAngle(z_axis, PI).v,
+                          3, PRECISE_EPS);
+      }
+    }
+
+    TEST(Vec3_rotate_aa)
+    {
+      {
+        Vec3 a{1.0, 1.0, 0.0};
+        CHECK_ARRAY_CLOSE(Vec3(-1.0, 1.0, 0.0).v,
+                          a.rotateAxisAngle(Vec3(0.0, 0.0, 1.0), PI*0.5).v,
+                          3, EPS);
+      }
+
+      {
+        Vec3 b{1.0, 1.0, 1.0};
+        CHECK_ARRAY_CLOSE(Vec3(-1.0, 1.0, 1.0).v,
+                          b.rotateAxisAngle(Vec3(0.0, 0.0, 1.0), PI*0.5).v,
+                          3, EPS);
+      }
+
+    }
+
+    TEST(Vec_assign)
+    {
+      Vec3 x{2.0, 4.0, 5.0};
+      Vec3 y = x;
+      CHECK_ARRAY_CLOSE(x.v, y.v, 3, PRECISE_EPS);
+    }
+
+    TEST(Vec_ref)
+    {
+      Vec3 x{2.0, 4.0, 5.0};
+      Vec3& y = x;
+      CHECK_ARRAY_CLOSE(x.v, y.v, 3, PRECISE_EPS);
+    }
+
+    TEST(Vec_rval)
+    {
+      Vec3 x{2.0, 4.0, 5.0};
+      Vec3 y = x;
+      Vec3 z = move(x);
+      CHECK_ARRAY_CLOSE(y.v, z.v, 3, PRECISE_EPS);
+    }
+
+    TEST(Vec_angle)
+    {
+      for (int i = 0; i < 20; ++i)
+      {
+        scalar theta = rf() * 2 * PI;
+        scalar phi = rf() * PI;
+        Vec3 x = Vec3::from_euler(theta, phi);
+        scalar t2, p2;
+        x.to_euler(t2, p2);
+        CHECK_CLOSE(theta, t2, EPS);
+        CHECK_CLOSE(phi, p2, EPS);
+      }
+    }
+  }
+}
