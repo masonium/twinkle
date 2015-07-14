f70bcc9cd77117be72882023f43f9ec4b2272646
Mason Smith <masonium@gmail.com>
2015-07-13 17:56:50 -0700

    Adds USE_DOUBLES define

--- src/twinkle.h
+++ src/twinkle.h
@@ -2,12 +2,19 @@
 
 #include <limits>
 
-using scalar = float;
+#ifdef USE_DOUBLES
+
+using scalar = double;
 const scalar EPSILON         = 0.0001;
+
+#else
+
+using scalar = float;
+const scalar EPSILON         = 0.00001;
+
+#endif
+
 const scalar SCALAR_MAX = std::numeric_limits<scalar>::max();
-
-
-
 
 
 
