7589d57849c694d8ef141a4c3fb9fbbcba0d2df2
Mason Smith <masonium@gmail.com>
2015-08-06 17:09:16 -0700

    Re-orients environment light so z-axis is up

--- src/env_light.cpp
+++ src/env_light.cpp
@@ -6,7 +6,7 @@ using std::tie;
 spectrum EnvironmentalLight::emission(const Vec3& dir) const
 {
   scalar theta, phi;
-  tie(theta, phi) = (Mat33::y_to_z * dir).to_euler();
+  tie(theta, phi) = dir.to_euler();
 
-  return texture->at_coord(Vec2(theta / (2*PI), 1.0 - phi / M_PI));
+  return texture->at_coord(Vec2(theta / (2*PI), phi / M_PI));
 }
