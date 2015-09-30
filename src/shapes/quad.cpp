75a5695f8cd1b8ebaad6ef6acded6f61cc702aa7
Mason Smith <masonium@gmail.com>
2015-09-29 23:35:20 -0700

    quad computes normals in method

--- src/shapes/quad.cpp
+++ src/shapes/quad.cpp
@@ -18,14 +18,16 @@ shared_ptr<Mesh> make_quad(const Vec3& xv, const Vec3& yv)
   vector<Vertex> vertices(4);
   Vec3 pos[4] = {-xv -  yv, xv - yv, -xv + yv, xv + yv};
   Vec2 uvs[4] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
+  const Vec3 n = xv.cross(yv).normal();
   for (auto i = 0u; i < 4; ++i)
   {
     vertices[i].position = pos[i];
     vertices[i].uv = uvs[i];
+    vertices[i].normal = n;
   }
   vector<Triangle> tris{{0, 1, 2}, {2, 1, 3}};
 
   RawModel model;
-  model.load_raw_model(vertices, tris, false, true);
+  model.load_raw_model(vertices, tris, true, true);
   return make_shared<Mesh>(model);
 }
