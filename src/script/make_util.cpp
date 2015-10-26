d197b3243f16b28b3b40ccb56fbafa894be549db
Mason Smith <masonium@gmail.com>
2015-10-26 01:33:37 -0700

    Fixes lua_tovector and lua_tospectrum

--- src/script/make_util.cpp
+++ src/script/make_util.cpp
@@ -10,13 +10,15 @@
 #include "script/make_material.h"
 
 using std::cerr;
+using std::make_shared;
 
 namespace script
 {
   template <typename T>
   int script_ptr(lua_State* L, shared_ptr<T> obj)
   {
-    auto ret = Manager<T>::instance().save(obj);
+    auto& mgr =  Manager<T>::instance();
+    auto ret = mgr.save(obj);
     auto user_data = lua_newuserdata(L, sizeof(ret));
     *reinterpret_cast<decltype(ret)*>(user_data) = ret;
     return 1;
@@ -27,7 +29,7 @@ namespace script
   {
     assert(lua_isuserdata(L, index));
 
-    auto mgr = Manager<T>::instance();
+    auto& mgr = Manager<T>::instance();
     using key_t = typename Manager<T>::key_type;
     key_t* user_data_key = reinterpret_cast<key_t*>(lua_touserdata(L, index));
     return mgr.at(*user_data_key);
@@ -78,14 +80,17 @@ namespace script
     return lua_toobj<Texture>(L, index);
   }
 
-  Vec3 lua_tovector(lua_State* L, int index)
+  template <typename T, typename Tag>
+  T lua_tovtype(lua_State* L, int index, Tag tag)
   {
     if (lua_istable(L, index))
     {
-      Vec3 v;
+      // translate the index to be positive
+      int pos_index = index > 0 ? index : (lua_gettop(L) + index + 1);
+      T v;
       for (auto i = 0u; i < 3; ++i)
       {
-        lua_rawgeti(L, index - i, i+1);
+        lua_rawgeti(L, pos_index, i+1);
         v[i] = lua_tonumber(L, -1);
       }
       lua_pop(L, 3);
@@ -98,24 +103,18 @@ namespace script
     }
   }
 
+  struct lua_tovector_tag {};
+  Vec3 lua_tovector(lua_State* L, int index)
+  {
+    lua_tovector_tag ltt;
+    return lua_tovtype<Vec3, lua_tovector_tag>(L, index, ltt);
+  }
+
+  struct lua_tospectrum_tag {};
   spectrum lua_tospectrum(lua_State* L, int index)
   {
-    if (lua_istable(L, index))
-    {
-      spectrum s;
-      for (auto i = 0u; i < 3; ++i)
-      {
-        lua_rawgeti(L, index -i, i+1);
-        s[i] = lua_tonumber(L, -1);
-      }
-      lua_pop(L, 3);
-      return s;
-    }
-    else
-    {
-      cerr << "value must be a table to convert to a spectrum\n";
-      exit(1);
-    }
+    lua_tospectrum_tag ltt;
+    return lua_tovtype<spectrum, lua_tospectrum_tag>(L, index, ltt);
   }
 
   int shape(lua_State* L)
@@ -133,8 +132,10 @@ namespace script
   {
     lua_register(L, "sphere", sphere);
     lua_register(L, "plane", plane);
+
     lua_register(L, "material_color", color_material);
     lua_register(L, "envlight_color", color_env_light);
+
     lua_register(L, "shape", shape);
   }
 }
