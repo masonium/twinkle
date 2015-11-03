bdf841b05132774daf2ae0f2d452e6bf6ad5c1ea
Mason Smith <masonium@gmail.com>
2015-11-02 18:28:13 -0800

    Removes old option parsing library code

--- tonemap_main.cpp
+++ tonemap_main.cpp
@@ -4,7 +4,6 @@
 #include <memory>
 #include "film.h"
 #include "reinhard.h"
-#include "optionparser.h"
 
 using std::string;
 using std::ifstream;
@@ -14,80 +13,14 @@ using std::endl;
 using std::make_shared;
 using std::shared_ptr;
 
-enum OptionNames{UNKNOWN, LINEAR, RSSF};
-
-option::ArgStatus optional_numeric(const option::Option& opt, bool msg)
-{
-  if (!opt.arg)
-    return option::ARG_OK;
-  char* endptr;
-  strtof(opt.arg, &endptr);
-  if (endptr == opt.arg)
-    return option::ARG_IGNORE;
-  return option::ARG_OK;
-}
-
-const option::Descriptor usage[] =
-{
-  {UNKNOWN, 0, "", "", option::Arg::None, "USAGE: tonemap [--linear/--rssf]\n\n"},
-  {LINEAR, 0, "l", "linear", option::Arg::None, ""},
-  {RSSF, 0, "r", "rssf", optional_numeric, "x"},
-  {UNKNOWN, 0, 0, 0, 0, 0}
-};
-
 int main(int argc, char** argv)
 {
-  argc -= (argc > 0);
-  argv += (argc > 0);
-  option::Stats stats(usage, argc, argv);
-  option::Option options[stats.options_max], buffer[stats.buffer_max];
-
-  option::Parser parse(usage, argc, argv, options, buffer);
-
-  assert(parse.nonOptionsCount() > 0);
-  string filename = parse.nonOption(0);
-  cerr << "Reading from " << filename << endl;
+  string filename(argv[0]);
 
   ifstream f(filename, std::ios::in | std::ios::binary);
-  if (!f)
-  {
-    cerr << "Could not read from " << filename << endl;
-    exit(1);
-  }
-
   Film film(f);
 
-  shared_ptr<ToneMapper> tonemap = make_shared<LinearToneMapper>();
-  if (options[LINEAR])
-  {
-    cerr << "Using linear tonemapper" << endl;
-    tonemap = make_shared<LinearToneMapper>();
-  }
-  else if (options[RSSF])
-  {
-    scalar mid = 0.18;
-    if (options[RSSF].arg)
-      mid = atof(options[RSSF].arg);
-    //tonemap = make_shared<RSSFToneMapper>(mid);
-    tonemap = make_shared<ReinhardGlobal>(mid);
-    cerr << "Using RSSF tonemapper with midscale of " << mid << endl;
-  }
-  else
-  {
-    cerr << "default to linear tonemapper." << endl;
-  }
+  auto tonemap = make_shared<ReinhardGlobal>(0.18);
 
   film.render_to_ppm(cout, *tonemap);
 }
-
-
-// int main()
-// {
-//   spectrum s{2.0, 3.0, 1.0};
-
-//   cout << "color: " << s << endl;
-//   cout << "old luminance: " << s.luminance() << endl;
-//   spectrum n = s.rescale_luminance(1.0);
-//   cout << "new spectrum: " << n << endl;
-//   cout << "new luminance: " << n.luminance() << endl;
-// }
