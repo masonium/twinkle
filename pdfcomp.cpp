32400e84c535934b4d8b8ee66df2ccf36b475279
Mason Smith <masonium@gmail.com>
2015-12-09 01:07:52 -0800

    Adds sampling tests
    tld - distribution tests
    rho_hd - hemispherical (ir?)radiance integral

--- pdfcomp.cpp
+++ pdfcomp.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <cassert>
+#include "materials/multilayered.h"
+#include "geometries.h"
 #include "sampler.h"
 #include "vec2.h"
 #include "bsdf.h"
@@ -105,16 +107,139 @@ void compare_bsdf(const BRDF& brdf, uint num_samples)
     cout << endl;
   }
   cout << total_integral / num_cells << endl << endl;
+}
 
+void compare_tld(const GTR& tld, uint num_samples)
+{
+  UniformSampler sampler;
+
+  const int theta_grid = 1;
+  const int phi_grid = 100;
+
+  sas r[theta_grid][phi_grid];
+
+  for (auto i = 0u; i < num_samples; ++i)
+  {
+    // compute the pdf
+    scalar p;
+    Vec3 dir = tld.sample_micronormal(sampler, p);
+
+    scalar theta, phi;
+    dir.to_euler(theta, phi);
+
+    int theta_i = theta / (2 * PI) * theta_grid ;
+    int phi_i = phi / (PI / 2) * phi_grid ;
+    assert(0 <= theta_i  && theta_i < theta_grid);
+    assert(0 <= phi_i  && phi_i < phi_grid);
+    r[theta_i][phi_i].n += 1;
+    r[theta_i][phi_i].tp += p;
+  }
+
+  const int num_cells = theta_grid * phi_grid;
+
+  scalar total_integral = 0;
+  for (int y = 0; y < theta_grid; ++y)
+  {
+    for (int x = 0; x < phi_grid; ++x)
+    {
+      scalar disp = num_cells * scalar(r[y][x].n) / num_samples;
+      cout << setw(12) << disp;
+      total_integral += disp;
+    }
+    cout << endl;
+  }
+  cout << total_integral / num_cells << endl << endl;
+
+  total_integral = 0;
+  for (int y = 0; y < theta_grid; ++y)
+  {
+    scalar t0 = y * 2 * PI / theta_grid;
+    scalar t1 = (y + 1)* 2 * PI / theta_grid;
+    for (int x = 0; x < phi_grid; ++x)
+    {
+      scalar p0 = x * 0.5 * PI / phi_grid;
+      scalar p1 = (x + 1) * 0.5 * PI / phi_grid;
+      scalar disp = r[y][x].n == 0 ? 0 : num_cells * (r[y][x].tp / r[y][x].n) * solid_angle_area(t0, t1, p0, p1);
+      cout << setw(12) << disp;
+      total_integral += disp;
+    }
+    cout << endl;
+  }
+  cout << total_integral / num_cells << endl << endl;
+
+
+  total_integral = 0;
+  for (int y = 0; y < theta_grid; ++y)
+  {
+    scalar t0 = y * 2 * PI / theta_grid;
+    scalar t1 = (y + 1)* 2 * PI / theta_grid;
+    for (int x = 0; x < phi_grid; ++x)
+    {
+      scalar p0 = x * 0.5 * PI / phi_grid;
+      scalar p1 = (x + 1) * 0.5 * PI / phi_grid;
+      scalar a = r[y][x].n == 0 ? 0 : num_cells * (r[y][x].tp / r[y][x].n) * solid_angle_area(t0, t1, p0, p1);
+      scalar b = num_cells * scalar(r[y][x].n) / num_samples;
+      scalar disp = a - b;
+      cout << setw(12) << disp * 100;
+      total_integral += disp;
+    }
+    cout << endl;
+  }
+  cout << total_integral / num_cells << endl << endl;
+}
+
+
+scalar compute_rho_hd(shared_ptr<Material> mat, const Vec3& incoming, uint num_samples)
+{
+  spectrum r;
+
+  auto p = make_shared<Plane>(Vec3(0, 0, 1), 0.0);
+  Shape s(p, mat);
+  Ray ray(incoming, -incoming);
+  UniformSampler sampler;
+
+  auto t = p->intersect(ray);
+  assert(t.is());
+  Intersection isect(&s, 0, ray, t.get());
+
+  uint valid_samples = 0;
+
+  for (auto i = 0u; i < num_samples; ++i)
+  {
+    scalar p;
+    spectrum reflectance;
+    Vec3 d = mat->sample_bsdf(isect, -ray.direction.normal(), sampler, p, reflectance);
+    if (p == 0)
+      continue;
+    r += reflectance / p * d.z;
+    ++valid_samples;
+  }
+
+  return (r / scalar(num_samples)).luminance();
 }
 
 int main(int argc, char** args)
 {
+  using namespace refraction_index;
+
   //compare_bsdf(OrenNayar(1.0, 0.3), 100000000);
-  UniformSampler sampler;
-  for (int i = 0; i < 1000; ++i)
+  //auto mat = make_shared<MirrorMaterial>();
+  auto base_mat = make_shared<RoughColorMaterial>(0.0, spectrum(0.0));
+  auto mat = make_shared<MaterialTLDAdapter>(AIR, CROWN_GLASS,
+                                             make_shared<GTR>(0.001));
+  vector<scalar> angles({0.0, 5.0, 10.0, 20.0, 30.0, 45.0, 60.0, 80.0, 85.0, 89.0});
+
+  for (auto deg: angles)
   {
-    auto v = uniform_sample_disc(sampler.sample_2d());
-    cout << v[0] << " " << v[1] << endl;
+    scalar angle = deg * PI / 180.0;
+    auto incoming = Vec3(sin(angle), 0.0, cos(angle)).normal();
+
+    cerr << setw(4) << deg << ": " << compute_rho_hd(mat, incoming, 1000000)
+         << " "
+         << fresnel_reflectance_schlick(incoming, Vec3::z_axis, AIR, CROWN_GLASS)
+         << endl;
   }
+
+//  compare_tld(GTR(0.1), 10000000);
+  return 0;
 }
