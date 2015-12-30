f8fd570c85d87a9a45655d4f23dbbbb6e5d80e73
Mason Smith <masonium@gmail.com>
2015-12-30 01:24:48 -0800

    update plans

--- assets/scripts/box.lua
+++ assets/scripts/box.lua
@@ -4,19 +4,18 @@ function scene()
    local m = material.dielectric()
 
    local shapes = {
-      shape(geom.translate(q1, {2.5, 0.0, 0.0}),
-		 material.glossy_paint(texture.color({0.1, 0.2, 0.6}))),
-      shape(geom.translate(q1, {2.5, 2.5, 0.0}), material.mirror()),
+      shape(q1, material.diffuse(texture.color({0.1, 0.2, 0.6}))),
+      shape(geom.translate(q1, {2.5, 2.5, 0.0}), material.dielectric()),
       shape(geom.translate(q1, {0.0, 2.5, 0.0}), material.diffuse(texture.color({0.8, 0.2, 0.3}))),
-      shape(geom.plane({0, 1, 0}, 0), material.diffuse(texture.grid({0.8}, {0.3}, 0.25, 0.05)))
+      shape(geom.plane({0, 0, 1}, 0), material.diffuse(texture.grid({0.8}, {0.3}, 0.25, 0.05)))
    }
 
-   local lights = { light.point({0, 2, 5}, {2.0}),
-		    light.environment(texture.color({0.1}))
+   local lights = { --light.point({0, 2, 5}, {2.0}),
+		    light.environment(texture.hoseksky({0.0, 4.0, 1.0}, 2.0))
    }
 
-   local pos = {4.0, 1.5, 4.3}
-   local camera = camera.perspective(pos, {0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, math.pi/2)
+   local pos = {6.0, 2.5, 1.25}
+   local camera = camera.perspective(pos, {0.0, 2.5, 1.25}, {0.0, 0.0, 1.0}, math.pi/2)
 
    return {shapes = shapes; lights = lights; camera = camera}
 end
