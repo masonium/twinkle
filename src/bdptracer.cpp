9b5a41ceccfe2b5f737c33d4c471feb7d38c019d
Mason Smith <masonium@gmail.com>
2016-01-03 18:06:42 -0800

    (WIP) BDPT implementation start

--- src/bdptracer.cpp
+++ src/bdptracer.cpp
@@ -7,45 +7,102 @@ BidirectionalPathTracer::BidirectionalPathTracer(const BDPTOptions& opt_)
 {
 }
 
-void BidirectionalPathTracer::render(const Camera* cam, const Scene* scene, Film& film)
+spectrum BidirectionalPathTracer::trace_ray(const Scene& scene, const Ray& ray, Sampler& s1, Sampler& s2) const
 {
+  auto eye_path = construct_eye_path(ray, scene, s1);
+
+  // Don't support light vertices connecting directly with the eye.
+  if (eye_path.trivial())
+    return eye_path.trivial_emission(scene);
+
+  auto light_path = construct_light_path(scene, s2);
+
+
+  return spectrum::zero;
 }
 
-void BidirectionalPathTracer::construct_light_path(Sampler& sampler, const Scene* scene,
-                                                   vector<PathVertex>& verts)
+BidirectionalPathTracer::EyePath BidirectionalPathTracer::construct_eye_path(
+  const Ray& eye_ray, const Scene& scene, Sampler& sampler) const
 {
+  EyePath path(eye_ray);
 
+  auto ray = eye_ray;
+
+  construct_path(scene, ray, 1.0, opt.eye_path_max_length, sampler, path);
+  return path;
 }
 
-void BidirectionalPathTracer::construct_eye_path(
-  const Film& film, int x, int y,
-  Sampler& sampler, const Camera* cam,
-  const Scene* scene, vector<PathVertex>& verts)
+BidirectionalPathTracer::LightPath BidirectionalPathTracer::construct_light_path(
+  const Scene& scene, Sampler& sampler) const
 {
-  auto ps = cam->sample_pixel(film.width, film.height, x, y, sampler);
-  verts.emplace_back(ps.ray.position, PV_EYE);
+  LightPath path;
+  auto emission_sample = scene.sample_emission(sampler);
+  path.add_light(emission_sample);
 
-  auto ray = ps.ray;
-  for (auto i = 0; i < opt.eye_path_max_length; ++i)
+  auto ray = emission_sample.ray.nudge();
+
+  construct_path(scene, ray, emission_sample.ray_prob,
+                 opt.light_path_max_length, sampler, path);
+  return path;
+}
+
+
+void BidirectionalPathTracer::construct_path(
+  const Scene& scene, const Ray& start_ray, scalar init_p, size_t max_length,
+  Sampler& sampler, Path& path) const
+{
+  // dir_p is the probability of hitting the next vertex (i.e. the probability
+  // of choosing the direction that hits that vertex). The first vertex is
+  // completely determined by our light sample.
+  scalar dir_p = init_p;
+
+  Ray ray = start_ray;
+  for (auto i = 0u; i < max_length; ++i)
   {
-    auto isect = scene->intersect(ray);
-    if (!isect)
-    {
-      verts.emplace_back(ray.direction.normal(), PV_ENV);
-      break;
-    }
+    // Interesct with the scene to find the next vertex in the path.
+    auto isect_opt = scene.intersect(ray);
 
-    //verts.emplace_back(isect);
+    // Stop if we hit nothing, or something resembling a light.
+    if (!isect_opt.is())
+      break;
+
+    const auto& isect = isect_opt.get();
+
     if (isect.is_emissive())
       break;
 
-    scalar brdf_p, refl;
+    // Stop probabilistic based on russian roulette.
+    if (opt.rr_min_length > 0 && i >= opt.rr_min_length)
+    {
+      if (sampler.sample_1d() < opt.rr_terminate_p)
+      {
+        break;
+      }
+      else
+      {
+        dir_p *= (1 - opt.rr_terminate_p);
+      }
+    }
+
+    // Sample to find the next point.
+    spectrum refl;
+    scalar brdf_p;
     Vec3 new_ray_dir = isect.sample_bsdf(-ray.direction.normal(), sampler, brdf_p, refl);
+    path.add_vertex(isect, refl, dir_p);
+
+    // The proability of this new ray is equal to the probabilty of the next ray.
+    dir_p = brdf_p;
     if (brdf_p == 0)
       break;
 
     ray = Ray(isect.position, new_ray_dir).nudge();
   }
 }
+
+spectrum BidirectionalPathTracer::EyePath::trivial_emission(const Scene& scene) const
+{
+  return scene.environment_light_emission(eye.ray.direction);
+}
+
 
 #endif
--- src/bdptracer.h
+++ src/bdptracer.h
@@ -1,53 +1,127 @@
 #pragma once
 
 #include "integrator.h"
+#include <vector>
+
+using std::vector;
 
 #ifdef FEATURE_BIDIRECTIONAL
 
 struct BDPTOptions
 {
-  int light_path_max_length;
-  int eye_path_max_length;
+  size_t light_path_max_length;
+  size_t eye_path_max_length;
   scalar power_heuristic_power;
+
+  // min path length before russian roulette starts.
+  size_t rr_min_length;
+
+  // probability of terminating at a given spot for russian roulette
+  scalar rr_terminate_p;
 };
 
 enum PVTag
 {
   PV_INTERSECTION = 0,
   PV_EYE,
+  PV_LIGHT,
   PV_ENV
 };
 
-
-class BidirectionalPathTracer : public Integrator
+class BidirectionalPathTracer : public RayIntegrator
 {
 public:
-  struct EnvironmentLightVertex
+  struct EyeVertex
   {
-    Vec3 direction;
+    EyeVertex(const Ray& r, scalar p) : ray(r), prob(p) { }
+
+    Ray ray;
+    scalar prob;
   };
 
   struct PathVertex
   {
-    PathVertex(Vec3 v, PVTag pvt) : vec(v), pv_tag(pvt) {}
+    PathVertex(const IntersectionView& view, const spectrum& refl, scalar p)
+      : isect_view(view), next_refl(refl), prob(p) { }
 
-    Vec3 vec;
-    PVTag pv_tag;
+    IntersectionView isect_view; // intersection information for this vertex
+
+    spectrum next_refl; // reflectance between the previous and next vertex
+                        // locations, not including the N.L components.
+
+    scalar prob; // probability that this vertex was chosen, given the previous
+                 // path up to this point
+  };
+
+  struct LightVertex
+  {
+    LightVertex() { }
+    LightVertex(const EmissionSample& es)
+      : emission_sample(es)
+    {
+    }
+
+    EmissionSample emission_sample;
+  };
+
+  struct Path
+  {
+    vector<PathVertex> path;
+
+    void add_vertex(const IntersectionView& isect, const spectrum& refl, scalar prob)
+    {
+      path.emplace_back(isect, refl, prob);
+    }
+  };
+
+  struct LightPath : public Path
+  {
+    LightPath() { }
+
+    LightVertex light;
+
+    void add_light(const EmissionSample& es)
+    {
+      light.emission_sample = es;
+    }
+  };
+
+  struct EyePath : public Path
+  {
+    EyePath(const Ray& r) : eye(r, 1.0)
+    {
+    }
+
+    EyeVertex eye;
+
+
+    bool trivial() const
+    {
+      return path.empty();
+    }
+
+    spectrum trivial_emission(const Scene&) const;
   };
 
   BidirectionalPathTracer(const BDPTOptions&);
 
-  void render(const Camera* cam, const Scene* scene, Film& film);
+  spectrum trace_ray(const Scene& scene, const Ray& ray, Sampler&, Sampler&) const;
 
   virtual ~BidirectionalPathTracer() { }
 
 private:
   spectrum sample_pixel(Sampler& sampler);
 
-  void construct_light_path(Sampler& sampler, const Scene* scene, vector<PathVertex>& verts);
-  void construct_eye_path(const Film& film, int x, int y,
-                          Sampler& sampler, const Camera* cam,
-                          const Scene* scene, vector<PathVertex>& verts);
+  /* Create a path from a random light. */
+  LightPath construct_light_path(const Scene& scene, Sampler& sampler) const;
+
+  /* Create a path starting from the camera at a particular point. */
+  EyePath construct_eye_path(const Ray& eye_ray, const Scene& scene,
+                             Sampler& sampler) const;
+
+  /* Create a path, given a set of starting conditions */
+  void construct_path(const Scene& scene, const Ray& start, scalar init_p,
+                      size_t max_length, Sampler& sampler, Path&) const;
 
   BDPTOptions opt;
 };
