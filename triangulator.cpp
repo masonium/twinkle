853ae84ebd1fd8dcd3dda47eb3fb1c2cf0b8f0c6
Mason Smith <masonium@gmail.com>
2016-02-07 21:49:23 -0800

    add built-in functions to triangulae in triangulator

--- triangulator.cpp
+++ triangulator.cpp
@@ -8,16 +8,34 @@
 using std::cout;
 using std::ofstream;
 
+scalar eight_surface(const Vec3& v)
+{
+  return 4.0 * square(square(v.z)) + 1.0 * (v.x*v.x + v.y*v.y - 4.0 * v.z*v.z);
+}
+
+scalar tanglecube(const Vec3& v) // -3 : 3
+{
+  Vec3 r = v.elem_mult(v);
+  return r.dot(r) - 5.0 * v.dot(v) + 11.8;
+}
+
+scalar heart(const Vec3& v) // -1.5 : 1.5
+{
+  scalar a = square(v.x) + 2.25 * square(v.y) + square(v.z) - 1;
+  scalar b = v.z*v.z*v.z;
+  return a * a * a - (square(v.x) + 9.0/80.0*square(v.y)) * b;
+
+}
+
 scalar f_eval(const Vec3& v)
 {
-  return v.norm2() - square(0.9);
+  return v.x *v.x + v.z * v.z - 0.5;
 }
 
 int main()
 {
-  //Vec3 base = Vec3{0.0, 0.0, 0.0};
-  Vec3 base = Vec3{-1.0};
-  MarchingTetrahedra mt(f_eval, bounds::AABB(base, base + Vec3{2.0}), 10);
+  scalar s = 3.0;
+  MarchingTetrahedra mt(tanglecube, bounds::AABB(Vec3{-s}, Vec3{s}), 30);
   auto model = mt.model();
   ofstream out("implicit.obj");
   model.save_to_obj(out);
